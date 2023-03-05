6a2218bc09e648892c36a9b31ab82c1e01170c66
mhasan8801 <hasanmuhammad197@gmail.com>
2023-03-05 08:44:54 +0700

    add section 11

---
+++ 11_Introduction Data Structures/Praktikum/script/script.js
@@ -0,0 +1,75 @@
+const productNameInput = document.getElementById("productName");
+const cekProductCategory = document.getElementById('productCategory');
+const cekImageProduct = document.getElementById('imageProduct');
+const cekProductFreshness = document.getElementsByName('productFreshness');
+const cekAdditional = document.getElementById('additionalProduct');
+const productPriceInput = document.getElementById("productPrice");
+
+const formProduct = document.getElementById('form-created-product');
+const tableProduct = document.getElementById('tableProduct');
+const tableBody = tableProduct.querySelector('tbody');
+
+
+const submitButton = document.getElementById("submit");
+
+
+submitButton.addEventListener("click", function (event) {
+    if (productNameInput.value === "" || productNameInput.value.length > 25) {
+        event.preventDefault();
+        alert("Product Name must not be empty and must not exceed 25 characters.");
+        return;
+    } else if (productNameInput.value.match(/[\#\{\}\@\/\(\)]/g) !== null) {
+        alert("Product name must not contain symbol.")
+    } else if (cekProductCategory.value === "") {
+        event.preventDefault();
+        alert("Product Category must not be empty.");
+        return;
+    } else if (cekImageProduct.value === "") {
+        event.preventDefault();
+        alert("Product Image must not be empty.");
+        return;
+    } else if (cekProductFreshness.value === "") {
+        event.preventDefault();
+        alert("Product Freshness must not be empty.");
+        return;
+    } else if (cekAdditional.value === "") {
+        event.preventDefault();
+        alert("Product Additional must not be empty.");
+        return;
+    } else if (productPriceInput.value === "") {
+        event.preventDefault();
+        alert("Product Price must not be empty.");
+        return;
+    } else {
+        alert("Product created successfully.");
+    }
+});
+
+
+formProduct.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formDataProduct = new FormData(formProduct);
+
+    const tableRow = tableBody.insertRow();
+
+    const productNameCell = tableRow.insertCell();
+    productNameCell.textContent = formDataProduct.get('productName');
+
+    const productCategoryCell = tableRow.insertCell();
+    productCategoryCell.textContent = formDataProduct.get('productCategory');
+
+    const productImageCell = tableRow.insertCell();
+    productImageCell.textContent = formDataProduct.get('imageProduct');
+
+    const productFreshnessCell = tableRow.insertCell();
+    productFreshnessCell.textContent = formDataProduct.get('productFreshness');
+
+    const productDescriptionCell = tableRow.insertCell();
+    productDescriptionCell.textContent = formDataProduct.get('additionalProduct');
+
+    const productPriceCell = tableRow.insertCell();
+    productPriceCell.textContent = formDataProduct.get('productPrice');
+
+    formProduct.reset();
+});
